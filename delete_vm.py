a95540f00fd96b57b8c1bb5c0c0aa44f5230d45e
Ossi05 <77546709+Ossi05@users.noreply.github.com>
2022-12-09 11:29:45 +0200

    Add files via upload

---
+++ delete_vm.py
@@ -0,0 +1,42 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.resource import ResourceManagementClient
+import configparser
+import os
+import time
+
+def main():
+
+
+
+    #Configs
+    config = configparser.ConfigParser()
+    config.read("config.ini")
+
+    #Azure_ID and Group
+    AZURE_ID = config.get('CONFIG', 'AZURE_ID')
+    group = config.get('CONFIG', 'RESOURCE_GROUP_NAME')
+
+
+    credentials = DefaultAzureCredential()
+    subscription_id = os.environ["AZURE_SUBSCRIPTION_ID"] = AZURE_ID
+    client = ResourceManagementClient(credentials, subscription_id)
+
+
+
+    try: 
+        print("Trying to delete VM and resources")
+        #Delete VM
+        delete_async_operation = client.resource_groups.begin_delete(group)
+        delete_async_operation.wait()
+        print("Deleting VM and resources")
+        delete_async_operation.wait()
+
+        print("Deleted VM and resources" )
+
+    except:
+        print("Error: You don't have any vm or resources to delete")
+        time.sleep(10)
+
+
+if __name__ == "__main__":
+    main()
